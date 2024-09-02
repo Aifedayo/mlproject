8c7722e70a5a371ab5076520a4c5423f3aa2f5db
Aifedayo <akeemifedayolag@gmail.com>
2024-09-02 12:48:27 +0100

    setup and package

--- setup.py
+++ setup.py
@@ -1,5 +1,21 @@
 # To create my machine learning model as a package
-from setup import find_packages, setup
+import os
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(filepath:str)->List[str]:
+    """
+    This function will return a list of requirments
+    """
+    with open('requirements.txt', 'r') as file:
+        requirements = file.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
 
 setup(
     name='mlproject',
@@ -7,5 +23,5 @@ setup(
     author='Akeem Lagundoye',
     author_email='akeemifedayolag@gmail.com',
     packages=find_packages(),
-    install_requirements=['pandas', 'numpy', 'seaborn']
+    install_requires=get_requirements('requirements.txt')
 )
