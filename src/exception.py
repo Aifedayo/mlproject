ca6e6de238fd0e1173417a64fa7079795ac2bf82
Aifedayo <akeemifedayolag@gmail.com>
2024-09-02 20:49:35 +0100

    Perfect the eception file and import logger

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,7 @@
 import sys
+import logging
+
+import logger
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -12,7 +15,15 @@ def error_message_detail(error, error_detail:sys):
 class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
         super().__init__(error_message)
-        self.error_message = error_message(error_message, error_detail=error_detail)
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
 
     def __str__(self) -> str:
         return self.error_message
+    
+
+if __name__ == '__main__':
+    try: 
+        a = 1/0
+    except Exception as e:
+        logging.info('Divide by Zero Error!')
+        raise CustomException(e, sys)
