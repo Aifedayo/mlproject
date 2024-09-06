356d79f81a877cbb008ad71851afeb993a84d004
Aifedayo <akeemifedayolag@gmail.com>
2024-09-06 21:54:00 +0100

    Use transform one the test data instead of fit transform

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -88,7 +88,7 @@ class DataTransformation:
                 f"Applying preprocessing object on training dataframe and testing dataframe"
             )
             input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
-            input_feature_test_arr = preprocessing_obj.fit_transform(input_feature_test_df)
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
 
             train_arr = np.c_[
                 input_feature_train_arr, np.array(target_feature_train_df)
