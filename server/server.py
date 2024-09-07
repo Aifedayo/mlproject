9a5121af32799302807de8673f7017adf11a3ade
Aifedayo <akeemifedayolag@gmail.com>
2024-09-07 09:48:10 +0100

    Build the predict pipeline and the server config

--- server/server.py
+++ server/server.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import PredictPipeline, CustomData
 
 app = Flask(__name__)
 
@@ -15,7 +16,25 @@ def index():
 
 @app.route('/predictdata', methods=['GET', 'POST'])
 def predict_datapoint():
-    if request.method == 'GET':
+    if request.method=='GET':
         return render_template('home.html')
     else:
-        pass
+        data=CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
+
+        )
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)
+        print("Before Prediction")
+
+        predict_pipeline=PredictPipeline()
+        print("Mid Prediction")
+        results=predict_pipeline.predict(features=pred_df)
+        print("after Prediction")
+        return render_template('home.html',results=results[0])
--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+import pickle
 
 from sklearn.metrics import r2_score
 
@@ -50,3 +51,13 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, params):
 
     except Exception as e:
         raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
