33041b137a26c7ea1df3dd0afb19fdb21e07af2f
Aifedayo <akeemifedayolag@gmail.com>
2024-09-07 08:25:03 +0100

    Added the home.html file

--- server/server.py
+++ server/server.py
@@ -11,3 +11,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/predictdata', methods=['GET', 'POST'])
+def predict_datapoint():
+    if request.method == 'GET':
+        return render_template('home.html')
+    else:
+        pass
